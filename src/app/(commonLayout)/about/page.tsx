f01ae311b3499ed89f91dba574c9548bbbd7c930
Yeakub1 <mryeakub10@gmail.com>
2024-07-06 18:05:30 +0600

    about

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -1,10 +1,7 @@
 "use client";
-import AboutSkills from "@/components/ui/About/AboutSkills/AboutSkills";
+
 import Education from "@/components/ui/About/Education/Education";
-import UserIntro from "@/components/ui/About/UserIntro/UserIntro";
-import LargeSpinner from "@/components/ui/Spinner/LargeSpinner";
-import Title from "@/components/ui/Title/Title";
-import { aboutDescription } from "@/constants/pageTitles";
+import Skills from "@/components/ui/Homepage/Skills/Skills";
 import { useGetUserProfileQuery } from "@/redux/features/user/userApi";
 import { useEffect } from "react";
 
@@ -24,16 +21,8 @@ const AboutPage = () => {
 
   return (
     <div>
-      <Title title="About" description={aboutDescription} route="About" />
-      <div className="xl:container mx-auto px-4 py-20">
-        {!userData && <LargeSpinner />}
-        <div className="mb-[100px]">
-          {userData && <UserIntro profile={profile} />}
-        </div>
-        {!userData && <LargeSpinner />}
-        <div className="mb-[100px]">{userData && <Education />}</div>
-        <AboutSkills />
-      </div>
+      <Skills />
+      <Education />
     </div>
   );
 };
--- src/components/ui/About/Education/Education.tsx
+++ src/components/ui/About/Education/Education.tsx
@@ -2,24 +2,33 @@ import { FaPen } from "react-icons/fa";
 
 const Education = () => {
   return (
-    <div
-      data-aos="fade-up"
-      className="border border-violet-950 p-5 lg:mx-20 shadow-inner shadow-violet-900/50 rounded-md"
-    >
-      <h2 className="flex items-center text-3xl text-violet-600 font-semibold mb-5">
-        Education <FaPen className="text-xl ml-2" />
-      </h2>
-      <p className="text-xl font-semibold text-violet-300 mb-2">
-        Kalinga Institute of Industrial Technology, India
-      </p>
-      <p className="font-semibold text-lg text-gray-400">
-        Computer Science and Engineering
-      </p>
-      <p className="text-gray-400 font-medium italic">
-        (Bachelor of Technology)
-      </p>
-      <p className="text-gray-400 font-medium mb-3">CGPA: 8.24</p>
-      <p className="font-semibold text-gray-400">Final Year (Running)</p>
+    <div className="bg-[#190128] text-white flex justify-center">
+      <div data-aos="fade-up" className="p-5 lg:mx-20 shadow-inner rounded-md">
+        <h2 className="flex items-center text-3xl font-semibold mb-5">
+          Diploma
+        </h2>
+        <p className="text-xl font-semibold text-violet-300 mb-2">
+          Ideal Polytecnich Instutite, Kushtia
+        </p>
+        <p className="font-semibold text-lg text-gray-400">
+          Diploma In Engineering Computer Technology
+        </p>
+        <p className="text-gray-400 font-medium italic">Computer Technology</p>
+        <p className="text-gray-400 font-medium mb-3">CGPA: 3.81</p>
+        <p className="font-semibold text-gray-400">2018-2023</p>
+      </div>
+      <div data-aos="fade-up" className="p-5 lg:mx-20 shadow-inner rounded-md">
+        <h2 className="flex items-center text-3xl font-semibold mb-5">
+          Secondary School Certificate 
+        </h2>
+        <p className="text-xl font-semibold text-violet-300 mb-2">
+          Chandiput secondary School
+        </p>
+       
+        <p className="text-gray-400 font-medium italic">Science</p>
+        <p className="text-gray-400 font-medium mb-3">GPA: 3.76</p>
+        <p className="font-semibold text-gray-400">2013-2018</p>
+      </div>
     </div>
   );
 };
